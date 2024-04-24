d275b2e928d261f146ce9faa0b994f8ced77ff21
shovovai <137371525+shovovai@users.noreply.github.com>
2024-04-24 10:59:04 +0600

    Add files via upload

---
+++ src/components/Contacts.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from 'react' 
+import CustomHook from './CustomHook';
+
+function Contacts() {
+const [listContacts] = useState([
+  {
+    title: 'Phone Number',
+    value: '+880 1614-865363'
+  },{
+    title: 'Email',
+    value: 'sakibullah981@engineer.com'
+  },{
+    title: 'Facebook',
+    value: 'md.sakibullah01'
+  }
+])
+const divs = useRef([]);
+const scrollTab = useRef();
+CustomHook(scrollTab, divs);
+
+  return (
+    <section className='contacts' ref={scrollTab}>
+       <div className="title" ref={(el) => el && divs.current.push(el)}>
+        This is my Contacts
+       </div>
+       <div className="des" ref={(el) => el && divs.current.push(el)}>
+        {/* 20 */}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis quae veniam amet nesciunt voluptatibus quis consectetur consequatur quisquam harum.
+       </div>
+       <div className="list"  ref={(el) => el && divs.current.push(el)}>
+        {
+          listContacts.map((value, key) => (
+            <div className='item' key={key}>
+              <h3>{value.title}</h3>
+              <div>{value.value}</div>
+            </div>
+          ))
+        }
+       </div>
+    </section>
+  )
+}
+export default Contacts
---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react' 
+import CustomHook from './CustomHook';
+function Home() {
+const scrollTab = useRef();
+CustomHook(scrollTab);
+
+return (
+    <section ref={scrollTab} className='home'>
+        <div className="content">
+            <div className="name">
+                MY NAME IS <span>Md.Sakibullah</span>
+            </div>
+            <div className="des">
+                {/* 30 */}
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores officiis beatae repellendus rem ullam, ipsam nemo dolorem dolorum illo laborum. Ea sed dolor ab qui, doloremque accusantium esse blanditiis possimus!
+            </div>
+            
+            <a href="/CV_Sakibullah.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
+                Download My CV
+            </a>
+        </div>
+        <div className="avatar">
+            <div className="card">
+                <img src="/avatar.jpg" alt="" />
+                <div className="info">
+                    <div>Developer</div>
+                    <div>Bangladeshi</div>
+                    <div>03/12</div>
+                    <div>Male</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Home
